9a0d5e50be53823387d1f3b71fd527bf3bbf38da
jmcorbera <jmcorbera@gmail.com>
2020-12-17 20:04:59 -0300

    mongo connect

--- server/src/mongodbConnModule.js
+++ server/src/mongodbConnModule.js
@@ -1,5 +1,6 @@
 var mongoose = require('mongoose');
 const uri = "mongodb+srv://UsuarioGeneral:mycar1234@cluster0.7xuhv.mongodb.net/MyCar?retryWrites=true&w=majority";
+//const uri = "mongodb://localhost:27017/MEVN-boilerplate";
 
 module.exports.connect = function() {
 	mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true });
